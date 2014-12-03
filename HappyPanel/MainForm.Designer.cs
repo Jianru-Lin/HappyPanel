26fdc2344ed9d7dd0bff417fa50bd2d265339260
Jianru Lin <jianru.lin@gmail.com>
2014-12-03 17:11:47 +0800

    初步可用

---
+++ HappyPanel/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace HappyPanel
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.logRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.argTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 64);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start(&1)";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(93, 64);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop(&2)";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.restartButton.Enabled = false;
+            this.restartButton.Location = new System.Drawing.Point(360, 64);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(75, 23);
+            this.restartButton.TabIndex = 4;
+            this.restartButton.Text = "Restart(&3)";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // logRichTextBox
+            // 
+            this.logRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logRichTextBox.Location = new System.Drawing.Point(12, 93);
+            this.logRichTextBox.Name = "logRichTextBox";
+            this.logRichTextBox.ReadOnly = true;
+            this.logRichTextBox.Size = new System.Drawing.Size(423, 65);
+            this.logRichTextBox.TabIndex = 4;
+            this.logRichTextBox.TabStop = false;
+            this.logRichTextBox.Text = "";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileTextBox.Location = new System.Drawing.Point(12, 12);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(423, 20);
+            this.fileTextBox.TabIndex = 0;
+            // 
+            // argTextBox
+            // 
+            this.argTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.argTextBox.Location = new System.Drawing.Point(12, 38);
+            this.argTextBox.Name = "argTextBox";
+            this.argTextBox.Size = new System.Drawing.Size(423, 20);
+            this.argTextBox.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 170);
+            this.Controls.Add(this.argTextBox);
+            this.Controls.Add(this.logRichTextBox);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.fileTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.ShowInTaskbar = false;
+            this.Text = "HappyPanel";
+            this.TopMost = true;
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Enter += new System.EventHandler(this.MainForm_Enter);
+            this.Leave += new System.EventHandler(this.MainForm_Leave);
+            this.MouseEnter += new System.EventHandler(this.MainForm_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.MainForm_MouseLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.RichTextBox logRichTextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.TextBox argTextBox;
+
+    }
+}
+
---
+++ HappyPanel/MainForm.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HappyPanel.Properties;
+using System.Diagnostics;
+
+namespace HappyPanel
+{
+    public partial class MainForm : Form
+    {
+        Process process;
+        bool restartSignal;
+
+        class Info
+        {
+            public Info(string filename, string argument)
+            {
+                this.filename = filename;
+                this.argument = argument;
+            }
+
+            public string filename;
+            public string argument;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        void log(string text)
+        {
+            logRichTextBox.AppendText(text + "\n");
+        }
+
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+            this.Opacity = 1;
+        }
+
+        private void MainForm_Deactivate(object sender, EventArgs e)
+        {
+            this.Opacity = 0.5;
+        }
+
+        private void MainForm_MouseEnter(object sender, EventArgs e)
+        {
+            //this.Opacity = 1;
+        }
+
+        private void MainForm_MouseLeave(object sender, EventArgs e)
+        {
+            //this.Opacity = 0.5;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.Save();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            start();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Info info = e.Argument as Info;
+
+            try
+            {
+                process = Process.Start(info.filename, info.argument);
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                e.Result = ex;
+            }
+            finally
+            {
+                process = null;
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            restartButton.Enabled = false;
+
+            Exception ex = e.Result as Exception;
+            if (ex != null)
+            {
+                log(ex.Message);
+            }
+            else
+            {
+                log("process exited");
+            }
+
+            if (restartSignal)
+            {
+                restartSignal = false;
+                start();
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stop();
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            restart();
+        }
+
+        void start()
+        {
+            string file = fileTextBox.Text;
+            if (string.IsNullOrWhiteSpace(file)) return;
+
+            string arg = argTextBox.Text;
+
+            log("[start] " + file + " " + arg);
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            restartButton.Enabled = true;
+
+            backgroundWorker.RunWorkerAsync(new Info(file, arg));
+        }
+
+        void stop()
+        {
+            log("[stop]");
+
+            if (process == null) return;
+            try
+            {
+                process.Kill();
+            }
+            catch (Exception ex)
+            {
+                log(ex.Message);
+            }
+        }
+
+        void restart()
+        {
+            log("[restart]");
+
+            startButton.Enabled = false;
+            stopButton.Enabled = false;
+            restartButton.Enabled = false;
+
+            stop();
+            restartSignal = true;
+        }
+
+        private void MainForm_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Rectangle rect = Screen.PrimaryScreen.WorkingArea;
+            Size size = this.Size;
+            this.Location = new Point(rect.Width - size.Width - 10, rect.Height - size.Height - 10);
+        }
+    }
+}
